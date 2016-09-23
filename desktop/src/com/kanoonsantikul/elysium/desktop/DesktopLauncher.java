6f362e4d83ebb21398b53076a0884cd32a26144d
kanoonsantikul <niti.sa@ku.th>
2016-09-23 11:26:35 +0700

    init project

---
+++ desktop/src/com/kanoonsantikul/elysium/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.kanoonsantikul.elysium.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kanoonsantikul.elysium.Elysium;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 600;
+		new LwjglApplication(new Elysium(), config);
+	}
+}
