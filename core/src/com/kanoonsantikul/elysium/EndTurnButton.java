721b19bb7ab7bbb626ba48af6374ecd146b9fc80
kanoonsantikul <nitisantikul@gmail.com>
2016-10-07 11:55:38 +0700

    Turn can swap

--- core/src/com/kanoonsantikul/elysium/EndTurnButton.java
+++ core/src/com/kanoonsantikul/elysium/EndTurnButton.java
@@ -9,11 +9,8 @@ public class EndTurnButton extends GameObject{
     private static final float X = Elysium.WIDTH - WIDTH / 2 - World.BOARD_X_INIT;
     private static final float Y = HEIGHT / 2 + 12;
 
-    private boolean isPlayer1Turn;
-
-    public EndTurnButton(boolean isPlayer1Turn){
+    public EndTurnButton(){
         setCenter(new Vector2(X, Y));
-        toggle(true);
     }
 
     public float getWidth(){
@@ -22,9 +19,5 @@ public class EndTurnButton extends GameObject{
 
     public float getHeight(){
         return HEIGHT;
-    }
-
-    public void toggle(boolean isPlayer1Turn){
-        this.isPlayer1Turn = isPlayer1Turn;
     }
 }
