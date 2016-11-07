b0d66e6549ceac657c0b1a7259be1756a62e72eb
kanoonsantikul <nitisantikul@gmail.com>
2016-11-07 15:21:34 +0700

    Fix multiplayer bug

--- core/src/com/kanoonsantikul/elysium/trap/BloodBank.java
+++ core/src/com/kanoonsantikul/elysium/trap/BloodBank.java
@@ -2,9 +2,9 @@ package com.kanoonsantikul.elysium;
 
 public class BloodBank extends Trap {
     public static final int ID = 10;
-    public static final float WEIGHT = 0.07f;
+    public static final float WEIGHT = 0.06f;
     public static final int COST = 5;
-    private static final int HEALTH = 200;
+    private static final int HEALTH = 150;
 
     public BloodBank (Tile tile, Player user) {
         super(ID, WEIGHT, COST, tile, user);
